62fc9c41358ee6681055bd2a09ad03205eae67be
agustinrojas <rojas.agustint@gmail.com>
2026-05-10 02:56:47 -0300

    first commit

---
+++ frontend/src/app/routes/ProtectedRoutes.tsx
@@ -0,0 +1,70 @@
+import { Route } from 'react-router'
+
+import { ProtectedLayout } from '../../components/ProtectedLayout'
+import { ProtectedRoute } from '../../components/ProtectedRoute'
+import { DashboardPage } from '../../pages/DashboardPage'
+import CreateNewsletterPage from '../../pages/CreateNewsletterPage'
+import EditNewsletterPage from '../../pages/EditNewsletterPage'
+import { EditPage } from '../../pages/EditPage'
+//import { EditTemplatePage } from '../../pages/EditTemplatePage'
+import { SettingsPage } from '../../pages/SettingsPage'
+
+export const protectedRoutes = (
+    <>
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <ProtectedLayout>
+              <DashboardPage />
+            </ProtectedLayout>
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/crearNewsletter"
+        element={
+          <ProtectedRoute>
+            <ProtectedLayout>
+              <CreateNewsletterPage />
+            </ProtectedLayout>
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/editarNewsletter/:id"
+        element={
+          <ProtectedRoute>
+            <ProtectedLayout>
+              <EditNewsletterPage />
+            </ProtectedLayout>
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/editarTemplate/:id"
+        element={
+          <ProtectedRoute>
+            <ProtectedLayout>
+              {/*<EditTemplatePage />*/}
+              <EditPage />
+            </ProtectedLayout>
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/settings"
+        element={
+          <ProtectedRoute>
+            <ProtectedLayout>
+              <SettingsPage />
+            </ProtectedLayout>
+          </ProtectedRoute>
+        }
+      />
+    </>
+)
